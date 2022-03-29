60c16fa5cad6a0116c5585ea468583558c0bc94a
Ted McGinness <ttmcginness22@gmail.com>
2022-03-29 10:46:26 -0500

    routes set up

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Transaction;
+
+class TransactionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Transaction::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'vendor' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+
+        ]);
+        return Transaction::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Transaction::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $transaction = Transaction::find($id);
+        $transaction->update($request->all());
+        return $transaction;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Transaction::destroy($id);
+    }
+
+
+    /**
+     * Search for a transaction
+     *
+     * @param  str  $vendor
+     * @return \Illuminate\Http\Response
+     */
+    public function search($vendor)
+    {
+        return Transaction::where('vendor', 'like', '%' . $vendor . '%')->get();
+    }
+}
